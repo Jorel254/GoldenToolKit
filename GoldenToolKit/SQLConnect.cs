e9204579f0e41c1fe39e1d67eb6e5cc30ca23daf
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2021-11-20 15:22:45 -0600

    Added ExecCommandSP method

---
+++ GoldenToolKit/SQLConnect.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace GoldenToolKit
+{
+    public class SQLConnect
+    {
+
+        public SqlConnection Connection { get;  set; }
+        public SqlCommand Command { get;  set; }
+        public SQLConnect() 
+        { 
+        }
+
+
+        public void ConnectionDB(string BaseName, string Server, string Port = "1433", string User = null, string Password = null)
+        {
+            Connection = new SqlConnection(BuildConnection(BaseName, Server, Port, User, Password));
+            try
+            {
+                Connection.Open();
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException(e.ToString());
+            }
+
+        }
+        public void DesconnectionDB(string BaseName, string Server, string Port = null, string User = null, string Password = null)
+        {
+            Connection = new SqlConnection(BuildConnection(BaseName, Server, Port, User, Password));
+            try
+            {
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException(e.ToString());
+            }
+
+        }
+        public void ExecCommandSP(SqlConnection connection,string SP, params SqlParameter[] parameters)
+        {
+            Command = new SqlCommand(SP, connection)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            foreach (SqlParameter param in parameters)
+            {
+                Command.Parameters.AddWithValue(param.ParameterName,param.Value);
+            }
+            Command.ExecuteNonQuery();
+        }
+        private static string BuildConnection(string BaseName, string Server, string Port, string User, string Password)
+        {
+            string cadena="";
+            cadena += "Data Source=";
+            cadena += Server + "\\SQLEXPRESS";
+            cadena += !string.IsNullOrEmpty(Port) ? "," + Port + ";" : ";";
+            cadena += "Initial Catalog=" + BaseName;
+            if (string.IsNullOrEmpty(User) && string.IsNullOrEmpty(Password))
+            {
+                cadena += ";Integrated Security=True;Trusted_Connection=True";
+            }
+            else
+            {
+                cadena += $";Integrated Security=false;Persist Security Info=True;User ID={User};Password={Password};";
+            }
+
+            return cadena;
+        }
+    }
+}
